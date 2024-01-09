a540578c9c2a046a7d9469f91c527853a1f5bbcf
Immanuel <solomon-santhosh.immanuel@worldline.com>
2024-01-09 15:59:36 +0530

    changes

--- app/src/main/java/com/example/applicationselectionwl/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/applicationselectionwl/ui/screens/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.applicationselectionwl.ui.screens
+package com.example.applicationselectionwl
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,9 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.*
-
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.applicationselectionwl.ui.screens.AbortScreen
+import com.example.applicationselectionwl.ui.screens.SelectionScreen
+import com.example.applicationselectionwl.ui.screens.WaitScreen
 import com.example.applicationselectionwl.ui.theme.ApplicationSelectionWLTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +24,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ApplicationSelectionWLTheme {
                 val navController = rememberNavController()
+
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -27,22 +34,45 @@ class MainActivity : ComponentActivity() {
                     Box(modifier = Modifier.fillMaxWidth()) {
                         NavHost(
                             navController = navController,
-                            startDestination = "SelectionScreen",
-                        ) {
-                            composable(route = "SelectionScreen") {
+                            startDestination = "waitScreen",
+
+                            ) {
+
+                            //post
+
+
+                            composable(route ="selectionScreen") {
                                 SelectionScreen()
                             }
-                            composable(route = "WaitScreen") {
+                            composable(route = "waitScreen") {
                                 WaitScreen()
                             }
-                            composable(route = "AbortScreen") {
+                            composable(route = "abortScreen") {
                                 AbortScreen()
                             }
 
                         }
                     }
+
                 }
+
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ApplicationSelectionWLTheme {
+        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/applicationselectionwl/ui/screens/WaitScreen.kt
+++ app/src/main/java/com/example/applicationselectionwl/ui/screens/WaitScreen.kt
@@ -1,8 +1,41 @@
 package com.example.applicationselectionwl.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun WaitScreen() {
+
+    Column(
+        
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+
+    ) {
+
+
+        Text(text = "Worldline",
+
+            color = Color.White,
+            modifier = Modifier.padding(top = 5.dp),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+
+        Text(text = "Please wait")
+
+
+
+    }
 }
