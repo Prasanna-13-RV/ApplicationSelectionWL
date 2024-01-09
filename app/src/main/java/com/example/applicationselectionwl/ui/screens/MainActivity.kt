0ae7c250d4e12226935317d5fc05c4a0d73b33d5
Immanuel <solomon-santhosh.immanuel@worldline.com>
2024-01-09 18:32:22 +0530

    loading over

--- app/src/main/java/com/example/applicationselectionwl/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/applicationselectionwl/ui/screens/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.applicationselectionwl.data.dataClasses.ApplicationModel
 import com.example.applicationselectionwl.ui.theme.ApplicationSelectionWLTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +24,7 @@ class MainActivity : ComponentActivity() {
             ApplicationSelectionWLTheme {
                 val navController = rememberNavController()
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
 
                     Box(modifier = Modifier.fillMaxWidth()) {
@@ -34,8 +36,17 @@ class MainActivity : ComponentActivity() {
                             composable(route = "SelectionScreen") {
                                 SelectionScreen()
                             }
-                            composable(route = "WaitScreen") {
-                                WaitScreen()
+                            composable(
+                                route = "WaitScreen/{applicationModel}",
+                                arguments = listOf(navArgument("applicationModel") {
+                                    type = NavType.ParcelableType(ApplicationModel::class.java)
+                                })
+                            ) { backStackEntry ->
+                                val application =
+                                    backStackEntry.arguments?.getParcelable<ApplicationModel>("applicationModel")
+
+                                WaitScreen(data = application!!)
+
                             }
                             composable(route = "AbortScreen") {
                                 AbortScreen()
@@ -43,9 +54,7 @@ class MainActivity : ComponentActivity() {
 
                         }
                     }
-
                 }
-
             }
         }
     }
--- app/src/main/java/com/example/applicationselectionwl/ui/screens/WaitScreen.kt
+++ app/src/main/java/com/example/applicationselectionwl/ui/screens/WaitScreen.kt
@@ -1,6 +1,7 @@
 package com.example.applicationselectionwl.ui.screens
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,23 +10,38 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.applicationselectionwl.R
+import com.example.applicationselectionwl.data.dataClasses.ApplicationModel
 import com.example.applicationselectionwl.ui.theme.GreenWL
+import kotlinx.coroutines.delay
 
 
 @Composable
-fun WaitScreen() {
+fun WaitScreen(data: ApplicationModel) {
+    var showContent by remember { mutableStateOf(false) }
     val strokeWidth = 5.dp
+
+
+    LaunchedEffect(Unit) {
+        // Delay for 3 seconds before showing the content
+        delay(3000)
+        showContent = true
+    }
 
 
     Column(
@@ -38,13 +54,12 @@ fun WaitScreen() {
     ) {
 
 
-        Text(
-            text = stringResource(id = R.string.worldline),
-
-            color = Color.White,
-            modifier = Modifier.padding(top = 20.dp),
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
+        Image(
+            painter = painterResource(R.drawable.worldline_logo),
+            contentDescription = stringResource(id = R.string.worldline),
+            modifier = Modifier
+                .width(200.dp)
+                .padding(top = 30.dp),
         )
 
         Column(
@@ -52,13 +67,19 @@ fun WaitScreen() {
             horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
                 .fillMaxSize()
         ) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(120.dp),
-                color = Color.White,
-                strokeWidth = strokeWidth
-            )
-            Spacer(modifier = Modifier.height(30.dp))
-            Text(text = stringResource(id = R.string.wait), color = Color.White)
+
+            if (showContent) {
+                Text(text = "Master card", color = Color.White)
+            } else {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(120.dp),
+                    color = Color.White,
+                    strokeWidth = strokeWidth
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = stringResource(id = R.string.wait), color = Color.White)
+
+            }
         }
 
 
