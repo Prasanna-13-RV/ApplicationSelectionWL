a7a63603571d85115bb1eda28b3ce477d26903d0
Prasanna <prasannavelmurugan0200@gmail.com>
2024-01-10 10:10:29 +0530

    abort screen changes

--- app/src/main/java/com/example/applicationselectionwl/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/applicationselectionwl/ui/screens/MainActivity.kt
@@ -38,7 +38,6 @@ class MainActivity : ComponentActivity() {
                             composable(route = "AbortScreen") {
                                 AbortScreen()
                             }
-
                         }
                     }
                 }
